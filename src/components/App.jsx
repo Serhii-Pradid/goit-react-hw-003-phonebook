3df28702a24d82009b6901d52b49bfbe6906a5cd
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-05-13 10:37:50 +0300

    completed task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,7 @@ import './App.module.css'
 
 export class App extends Component {
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [],
     filter: '',
     };
 
@@ -45,10 +41,28 @@ export class App extends Component {
     const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
-     }
-    
 
 
+     };
+
+     componentDidMount() {
+  const contacts = localStorage.getItem('contacts')      // берем з localStorage збережені дані контактів
+  const parseContacts = JSON.parse(contacts)             // робимо з них масив
+  
+  console.log(parseContacts)
+
+  if (parseContacts) {
+    this.setState({contacts: parseContacts});            // і записуємо в state збережені дані із localStorage
+}
+  }
+     
+     componentDidUpdate(prevProps, prevState) {
+      if (this.state.contacts !== prevState.contacts) {                         // порівнюємо попедедній та новий масиви і якщо вони не рівні 
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts))   // то записуємо новий масив в localStorage
+      }
+     };
+
+     
 render() {
   const {filter} = this.state;
     
